918a4d280d1a29d3ef559acfa2870021f9213167
apoplutin <apoplutin@foundationmedicine.com>
2022-01-03 22:48:21 +0200

    Исправления методовremove not needed sln file

--- Array Resize Method/Program.cs
+++ Array Resize Method/Program.cs
@@ -3,66 +3,83 @@
 namespace Array_Resize_Method
 {
     class Program
+{
+
+    static void Resize(ref int[] Array, int Length)
     {
-
-        static void Resize(ref int[] NewArray, int Length)
+        int[] NewArray = new int[Length];
+        for (int i = 0; i < Array.Length && i < NewArray.Length; i++)
         {
-            NewArray = new int[Length];
+            NewArray[i] = Array[i];
         }
 
-        static void ChangeArrElem(ref int[] Arr, int index, int Value)
+        Array = NewArray;
+    }
+
+    static void Insert(ref int[] Arr, int index, int Value)
+    {
+        int[] NewArray = new int[Arr.Length + 1];
+        NewArray[index] = Value;
+
+        for (int i = 0; i < index; i++)
+            NewArray[i] = Arr[i];
+        for (int i = index; i < Arr.Length; i++)
+            NewArray[i + 1] = Arr[i];
+        Arr = NewArray;
+
+    }
+
+    static void ChangeFirstElem(ref int[] Arr, int Value)
+    {
+        Insert(ref Arr, 0, Value);
+    }
+
+    static void ChangeLastElem(ref int[] Arr, int Value)
+    {
+        Insert(ref Arr, (Arr.Length), Value);
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1, 6, 5, 8, 7 };
+        Console.WriteLine("Default Array: ");
+        for (int i = 0; i < myArray.Length; i++)
         {
-            Arr[index] = Value;
+            Console.Write(myArray[i] + "\t");
+
         }
 
-        static void ChangeFirstElem(ref int[] Arr, int Value)
+        // Console.Write("\n" + "input new array length: " + "\t");
+        // int a = int.Parse(Console.ReadLine());
+
+        //Resize(ref myArray, a);
+
+
+        Console.Write("\n" + "Input Value of first element: " + "\t");
+        int FirstValue = int.Parse(Console.ReadLine());
+        ChangeFirstElem(ref myArray, FirstValue);
+
+        Console.Write("\n" + "Input Value of last element: " + "\t");
+        int LastValue = int.Parse(Console.ReadLine());
+        ChangeLastElem(ref myArray, LastValue);
+
+        Console.Write("\n" + "Input index of changed element: " + "\t");
+        int index = int.Parse(Console.ReadLine());
+        Console.Write("\n" + "Input Value of changed element: " + "\t");
+        int Value = int.Parse(Console.ReadLine());
+        Insert(ref myArray, index, Value);
+
+
+        Console.WriteLine("Your Modified Array: ");
+        for (int i = 0; i < myArray.Length; i++)
         {
-            Arr[0] = Value;
+            Console.Write(myArray[i] + "\t");
+
         }
 
-        static void ChangeLastElem(ref int[] Arr, int Value)
-        {
-            Arr[(Arr.Length - 1)] = Value;
-        }
-
-
-
-        static void Main(string[] args)
-        {
-            int[] myArray = new int[5];
-            Console.WriteLine("Default Array: ");
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                Console.Write(myArray[i] + "\t");
-
-            }
-
-            Console.Write("\n" + "input new array length: " + "\t");
-            int a = int.Parse(Console.ReadLine());
-
-            Resize(ref myArray, a);
-
-            Console.Write("\n" + "Input index of changed element: " + "\t");
-            int index = int.Parse(Console.ReadLine());
-
-            Console.Write("\n" + "Input Value of changed element: " + "\t");
-            int Value = int.Parse(Console.ReadLine());
-
-
-            ChangeArrElem(ref myArray, index, Value);
-
-
-            Console.Write("\n" + "Input changed Value of last element: " + "\t");
-            int lastValue = int.Parse(Console.ReadLine());
-            ChangeLastElem(ref myArray, lastValue);
-
-            Console.WriteLine("Your Modified Array: ");
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                Console.Write(myArray[i] + "\t");
-
-            }
-
-        }
     }
 }
+}
+
