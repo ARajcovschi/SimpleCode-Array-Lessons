ddb39ded5c941f72f0a0e28307e31b3ddb18148c
Alexandr Rajcovschi <96792948+PacKoJlbHuK@users.noreply.github.com>
2022-01-03 20:42:16 +0200

    Add files via upload

---
+++ Array Resize Method/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Array_Resize_Method
+{
+    class Program
+    {
+
+        static void Resize(ref int[] NewArray, int Length)
+        {
+             NewArray = new int[Length];
+        
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+            Console.Write(myArray[i]+"\t");
+
+            }
+
+            Console.Write("\n" + "Input New array Length: " + "\t");
+                int a = int.Parse(Console.ReadLine());
+            Resize(ref myArray, a);
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write(myArray[i] + "\t");
+
+            }
+
+        }
+    }
+}
