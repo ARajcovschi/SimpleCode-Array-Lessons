90570738916f56673064210dc440653aa5d360bf
Alexandr Rajcovschi <alexandr.rajcovschi@gmail.com>
2022-01-04 20:23:37 +0200

    Added new project for lesson 43

---
+++ CodeWars Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeWars_Test
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string Text = int
+
+
+        }
+    }
+}
---
+++ Delete Method/Array Delete Method.cs
@@ -0,0 +1,22 @@
+using System;
+using Linq;
+
+class Program
+    static void DeleteElem (ref int Array[], int index)
+{
+    newArray[]=
+    for (int i = 0; i < index; i++)
+    {
+
+    }
+}
+
+
+
+    static void main(string[] args)
+{
+
+
+
+}
+    
