2b0b61a5c19557a5fa8bc46c5e2eeb7980d7d24e
Alexandr Rajcovschi <alexandr.rajcovschi@gmail.com>
2022-02-15 21:41:34 +0200

    Aded new project for Currency Calc

---
+++ CurrencyCalculator/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCalculator
+{
+    class Class1
+    {
+        //Класс который совпадает с АПИ
+        //ГЕТ запрос с АПИ
+        //отдать текст в НьютонСофт
+        //работа с объектом
+    }
+}
---
+++ CurrencyCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CurrencyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
